cc3b61506c4e75cbed4bab8c3b2018aa0d01390c
Hila Shay <hilashay17.3@gmail.com>
2022-10-19 19:46:59 +0300

    review list rendered without carousel

--- src/modules/Reviews/Cards.tsx
+++ src/modules/Reviews/Cards.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { SectionContainer } from "../../components/General.styled";
 import Card from "./Card";
 
 const Cards = () => {
@@ -9,6 +8,12 @@ const Cards = () => {
       name: "Hila Shay",
       jobName: "CEO,word yt",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a laoreet libero dis eget maecenas bibendum. Morbi lacinia eu, etiam volutpat morbi et amet, sagittis adipiscing..",
+    },
+    {
+      imageUrl: "https://i.pinimg.com/564x/cf/32/5d/cf325d12d80120757b2e0b7db1e2f9a2.jpg",
+      name: "Idan Haviv",
+      jobName: "Managing Director",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a laoreet libero dis eget maecenas bibendum. Morbi lacinia eu, etiam volutpat morbi et amet, sagittis adipiscing. .",
     },
   ];
 
@@ -24,7 +29,8 @@ export default Cards;
 
 const Container = styled.div`
   display: flex;
-  margin-top: 56px;
+  padding: 56px 135px 135px 130px;
   flex-direction: row;
   justify-content: space-between;
+  width: 100%; // Question: on about i shouldnt add "width", but here without width theres no option to add space between the cards.****
 `;
