4ff30c95ae5077baadbb55eb4cdd823e88d46b08
Hila Shay <hilashay17.3@gmail.com>
2022-11-15 17:34:36 +0200

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styled from "styled-components";
-import About from "./modules/About/About";
+
 import DressMe from "./modules/DressMe/DressMe";
 import Footer from "./modules/Footer/Footer";
 import Header from "./modules/Header/Header";
-import Home from "./modules/Home/Home";
-import HowItWorks from "./modules/HowItWorks/HowItWorks";
-import Reviews from "./modules/Reviews/Reviews";
+import HomePage from "./modules/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="form-section" element={<DressMe />} />
       </Routes>
-      <AppContainer>
-        <Header />
-        <Home />
-        <HowItWorks />
-        <About />
-        <Reviews />
-        <Footer />
-      </AppContainer>
+      <Footer />
     </BrowserRouter>
   );
 }
 
 export default App;
-
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
