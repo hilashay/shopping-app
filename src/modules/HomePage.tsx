dbe62c18e4733637551b8e0d7bfd0ffccc404799
Hila Shay <hilashay17.3@gmail.com>
2022-11-15 17:30:52 +0200

    another navigation changes

---
+++ src/modules/HomePage.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import About from "./About/About";
+import Home from "./Home/Home";
+import HowItWorks from "./HowItWorks/HowItWorks";
+import Reviews from "./Reviews/Reviews";
+
+function HomePage() {
+  return (
+    <AppContainer>
+      <Home />
+      <HowItWorks />
+      <About />
+      <Reviews />
+    </AppContainer>
+  );
+}
+
+export default HomePage;
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
