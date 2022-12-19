15db708bf832f4376d6538ac8131613e4b1a6209
Hila Shay <hilashay17.3@gmail.com>
2022-12-19 14:00:45 +0200

    remove unsend stuff

--- src/modules/Footer/Social/Social.tsx
+++ src/modules/Footer/Social/Social.tsx
@@ -1,16 +1,9 @@
 import styled from "styled-components";
 import Icons from "./Icons";
-import { Span } from "../../../components/General.styled";
-const text = `Lorem ipsum dolor sit amet,
-consectetur adipiscing elit. Sit a
-laoreet libero dis eget maecenas
-bibendum.`;
 
 const Social = () => {
   return (
     <Container>
-      <Title>{/* <Span>Soci</Span>al */}</Title>
-      {/* <Text>{text}</Text> */}
       <Icons />
     </Container>
   );
@@ -24,15 +17,4 @@ const Container = styled.div`
   width: 263px;
   padding-right: 37px;
   padding-bottom: 60px;
-`;
-
-const Title = styled.h2`
-  font-size: 40px;
-`;
-
-const Text = styled.p`
-  height: 95px; // TRADE OFF: insted of give it 102 px I give it less to make the icons be closer and at the same line as the last link in "Menu" - "Contact us"
-  font-size: 16px;
-  padding-top: 25px;
-  white-space: pre;
 `;
